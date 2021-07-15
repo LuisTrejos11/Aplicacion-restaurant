587479d68b79ce2c1f2ef8c3f18592464b7de0b3
LuisTrejos11 <54832126+LuisTrejos11@users.noreply.github.com>
2021-07-14 23:52:50 -0600

    se sube el proyecto

---
+++ RestCsharp/Datos/Dpermisos.cs
@@ -0,0 +1,76 @@
+﻿using RestCsharp.Logica;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RestCsharp.Datos
+{
+   public  class Dpermisos
+    {
+        public bool Insertar_Permisos(Lpermisos parametros)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlCommand cmd = new SqlCommand("insertar_Permisos", CONEXIONMAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdModulo", parametros.IdModulo);
+                cmd.Parameters.AddWithValue("@IdUsuario", parametros.IdUsuario);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }    
+        public bool Eliminar_Permisos(Lpermisos parametros)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlCommand cmd = new SqlCommand("Eliminar_Permisos", CONEXIONMAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdUsuario", parametros.IdUsuario);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+        public void mostrar_Permisos(ref DataTable dt, Lpermisos  parametros)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlDataAdapter da = new SqlDataAdapter("mostrar_Permisos", CONEXIONMAESTRA.conectar);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddWithValue("@idusuario", parametros.IdUsuario);
+
+                da.Fill(dt);
+
+                CONEXIONMAESTRA.cerrar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ RestCsharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RestCsharp
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Presentacion.Usuarios.UsuariosOk  frm = new Presentacion.Usuarios.UsuariosOk();
+            frm.FormClosed += frm_closed;
+            frm.ShowDialog();
+            Application.Run();
+        }
+        private static void frm_closed(object sender, FormClosedEventArgs e)
+        {
+            Application.ExitThread();
+            Application.Exit();
+        }
+    }
+}
